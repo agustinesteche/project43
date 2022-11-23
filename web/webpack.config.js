f7869176f076e1adeeadacc6899166a83407b26e
Agustin Esteche <agustinesteche@gmail.com>
2022-11-23 13:13:09 -0300

    Add web.

---
+++ web/webpack.config.js
@@ -0,0 +1,79 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const appDirectory = path.resolve(__dirname, '../');
+const webDirectory = path.resolve(__dirname);
+const {presets} = require(`${webDirectory}/babel.config.js`);
+
+const compileNodeModules = [
+  // Add every react-native package that needs compiling
+  // 'react-native-gesture-handler',
+].map((moduleName) => path.resolve(appDirectory, `node_modules/${moduleName}`));
+
+const babelLoaderConfiguration = {
+  test: /\.js$|tsx?$/,
+  // Add every directory that needs to be compiled by Babel during the build.
+  include: [
+    path.resolve(webDirectory, 'index.web.js'), // Entry to your application
+    path.resolve(appDirectory, 'App.js'), // Change this to your main App file
+    path.resolve(appDirectory, 'src'),
+    ...compileNodeModules,
+  ],
+  use: {
+    loader: 'babel-loader',
+    options: {
+      cacheDirectory: true,
+      presets,
+      plugins: ['react-native-web'],
+    },
+  },
+};
+
+const svgLoaderConfiguration = {
+  test: /\.svg$/,
+  use: [
+    {
+      loader: '@svgr/webpack',
+    },
+  ],
+};
+
+const imageLoaderConfiguration = {
+  test: /\.(gif|jpe?g|png)$/,
+  use: {
+    loader: 'url-loader',
+    options: {
+      name: '[name].[ext]',
+    },
+  },
+};
+
+module.exports = {
+  entry: {
+    app: path.join(webDirectory, 'index.web.js'),
+  },
+  output: {
+    path: path.resolve(webDirectory, 'dist'),
+    publicPath: '/',
+    filename: 'bundle.web.js',
+  },
+  resolve: {
+    extensions: ['.web.tsx', '.web.ts', '.tsx', '.ts', '.web.js', '.js'],
+    alias: {
+      'react-native$': 'react-native-web',
+    },
+  },
+  module: {    
+    rules: [
+      babelLoaderConfiguration,
+      imageLoaderConfiguration,
+      svgLoaderConfiguration,
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(webDirectory, 'index.html'),
+    }),
+  ],
+};
