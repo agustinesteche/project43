cee76383049b83dc2bfd7e93cfd7c9b8967bbd02
Agustin Esteche <agustinesteche@gmail.com>
2022-11-27 10:29:26 -0300

    Add platfom indicator.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import {SafeAreaView, ScrollView, Text, View} from 'react-native';
+import {SafeAreaView, Text, View, Platform} from 'react-native';
+import Styles from './Styles';
 
 function App() {
   return (
     <SafeAreaView>
-      <ScrollView>
-        <View>
-          <Text>Project #43</Text>
-        </View>
-      </ScrollView>
+      <View style={Styles.content}>
+        <Text style={Styles.title}>PROJECT #43</Text>
+        <Text style={Styles.subtitle}>{`Running on ${Platform.OS}.`}</Text>
+      </View>
     </SafeAreaView>
   );
 }
---
+++ src/Styles.js
@@ -0,0 +1,19 @@
+import {StyleSheet} from 'react-native';
+
+const Styles = StyleSheet.create({
+  content: {
+    padding: 24,
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 22,
+    fontWeight: 'bold',
+    color: '#2c2d2f',
+  },
+  subtitle: {
+    fontSize: 14,
+    color: '#848484',
+  },
+});
+
+export default Styles;
